67d14c5a1a8bc5e96417aedd48c977ebc02d03d4
Eduardo Barros <edu.barros101@gmail.com>
2023-06-19 18:03:44 -0300

    Decorators resolvidos.

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -3,9 +3,8 @@ import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js'
 
 export abstract class View<T> {
   protected elemento: HTMLElement
-  private escapar = false
 
-  constructor(seletor: string, escapar?: boolean) {
+  constructor(seletor: string) {
     const elemento = document.querySelector(seletor)
 
     if (elemento) {
@@ -13,20 +12,12 @@ export abstract class View<T> {
     } else {
       throw Error(`Seletor ${seletor} não existe no DOM. Verifique.`)
     }
-
-    if (escapar) {
-      this.escapar = escapar
-    }
   }
 
   @inspect()
   @logarTempoDeExecucao(true)
   public update(model: T): void {
     let template = this.template(model)
-
-    if (this.escapar) {
-      template = template.replace(/<script>[\s\S]*?<\/script>/, '')
-    }
 
     this.elemento.innerHTML = template
   }
