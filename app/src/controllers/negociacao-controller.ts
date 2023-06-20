16065f3629017f28ee04ea46aa2ada88706a50ca
Eduardo Barros <edu.barros101@gmail.com>
2023-06-20 16:39:44 -0300

    Definicao de interface.

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { NegociacoesDoDia } from '../interfaces/negociacao-do-dia.js'
 import { domInjector } from '../decorators/dom-injector.js'
 import { inspect } from '../decorators/inspect.js'
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js'
@@ -41,8 +42,25 @@ export class NegociacaoController {
     this.atualizaView()
   }
 
-  importaDados(): void {
-    alert('Oi')
+  public importaDados(): void {
+    fetch('http://localhost:8080/dados')
+      .then((res) => res.json())
+      .then((dados: NegociacoesDoDia[]) => {
+        return dados.map((dadoDeHoje) => {
+          return new Negociacao(
+            new Date(),
+            dadoDeHoje.vezes,
+            dadoDeHoje.montante
+          )
+        })
+      })
+      .then((negociacoesDeHoje) => {
+        for (let negociacao of negociacoesDeHoje) {
+          this.negociacoes.adiciona(negociacao)
+        }
+
+        this.negociacoesView.update(this.negociacoes)
+      })
   }
 
   private eDiaUtil(data: Date) {
