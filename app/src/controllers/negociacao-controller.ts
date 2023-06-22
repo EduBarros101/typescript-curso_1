bd5958d472846c7146e92421f231ba1a453bcea3
Eduardo Barros <edu.barros101@gmail.com>
2023-06-22 17:36:14 -0300

    Chamada de Serviços.

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,4 +1,3 @@
-import { NegociacoesDoDia } from '../interfaces/negociacao-do-dia.js'
 import { domInjector } from '../decorators/dom-injector.js'
 import { inspect } from '../decorators/inspect.js'
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js'
@@ -7,6 +6,7 @@ import { Negociacao } from '../models/negociacao.js'
 import { Negociacoes } from '../models/negociacoes.js'
 import { MensagemView } from '../views/mensagem-view.js'
 import { NegociacoesView } from '../views/negociacoes-view.js'
+import { NegociacoesService } from '../services/negociacoes-service.js'
 
 export class NegociacaoController {
   @domInjector('#data')
@@ -18,6 +18,7 @@ export class NegociacaoController {
   private negociacoes = new Negociacoes()
   private negociacoesView = new NegociacoesView('#negociacoesView')
   private mensagemView = new MensagemView('#mensagemView')
+  private negociacoesService = new NegociacoesService()
 
   constructor() {
     this.negociacoesView.update(this.negociacoes)
@@ -43,22 +44,12 @@ export class NegociacaoController {
   }
 
   public importaDados(): void {
-    fetch('http://localhost:8080/dados')
-      .then((res) => res.json())
-      .then((dados: NegociacoesDoDia[]) => {
-        return dados.map((dadoDeHoje) => {
-          return new Negociacao(
-            new Date(),
-            dadoDeHoje.vezes,
-            dadoDeHoje.montante
-          )
-        })
-      })
+    this.negociacoesService
+      .obterNegociacoesDoDia()
       .then((negociacoesDeHoje) => {
         for (let negociacao of negociacoesDeHoje) {
           this.negociacoes.adiciona(negociacao)
         }
-
         this.negociacoesView.update(this.negociacoes)
       })
   }
